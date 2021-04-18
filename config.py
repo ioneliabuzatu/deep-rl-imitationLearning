8fa24b9b27ceb08777f388d37e6707a427f232e2
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-18 19:48:37 +0200

    [CLUSTER] !!pyglet

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ batch_size = 64
 
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
-    "",
+    "mila",
     sweep_yaml="",
     proc_num=1,
     wandb_kwargs={"entity": "ionelia"}
